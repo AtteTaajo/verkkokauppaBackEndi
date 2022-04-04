e05587a536b61001f6a9c900ef7cb7dca6584825
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-04-04 16:44:32 +0300

    update

--- inc/functions.php
+++ inc/functions.php
@@ -1,15 +1,10 @@
 <?php
 function openDb(): object {
     $ini= parse_ini_file("../config.ini", true);
-
-    var_dump($ini);
-
     $host = $ini["host"];
     $database = $ini["database"];
     $user = $ini["user"];
     $password = $ini["password"];
-
-
     $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$user,$password);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $db;
@@ -20,7 +15,7 @@ function selectAsJson(object $db, string $sql): void {
     $query = $db->query($sql);
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
     header("HTTP/1.1 200 OK");
-    echo json_encode($results);
+    echo json_encode($results, JSON_PRETTY_PRINT);
 }
 
 function executeInsert(object $db, string $sql): int {
