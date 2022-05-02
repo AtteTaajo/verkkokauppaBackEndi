7e6ae691b518a0368d9ac07bd00de55dfd29fafa
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-05-02 17:37:35 +0300

    imaget

--- webshop.sql
+++ webshop.sql
@@ -13,7 +13,7 @@ create table product (
     id int primary key AUTO_INCREMENT,
     name VARCHAR(255) not null, 
     price double(10,2) not null,
-    image VARCHAR(50),
+    image VARCHAR(255),
     category_id int not null,
     index category_id(category_id),
     FOREIGN KEY (category_id) REFERENCES category(id)
@@ -25,7 +25,7 @@ insert into category (name) values ("Red Bullit");
 insert into category (name) values ("Batteryt");
 
 insert into category (name) values ("Monsterit");
-INSERT into product (name, price, image, category_id) values ("Test Redbulli 1", 3.33,'teho_pullo.png', 1);
-INSERT into product (name, price, image, category_id) values ("Test Battery 1", 2.45, 'battery.png', 2);
+INSERT into product (name, price, image, category_id) values ("Test Redbulli 1", 3.33,'https://public.keskofiles.com/f/k-ruoka/product/9002490100773?w=480&h=480&fit=max&auto=format&fm=jpg&cs=srgb', 1);
+INSERT into product (name, price, image, category_id) values ("Test Battery 1", 2.45, 'https://alkostore24.com/media/catalog/product/cache/d93fad4313d058678385fbb1227fb376/7/0/7020447_1.jpg', 2);
 
 /* Tää vaa viskataa sinne mysql :) */ 
