7c229da5046b8d6f23c8df12dc613850d8844123
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-05-04 19:36:01 +0300

    hintoja hiema muuteltu

--- webshop.sql
+++ webshop.sql
@@ -31,39 +31,39 @@ insert into category (name) values ("Monsterit");
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull", 2.95,'https://i.imgur.com/xo3X864.jpeg', 'Original', '','', 1);
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull Pitaija", 2.95, 'https://i.imgur.com/sGhyZAd.png', 'Green nature', '', '',  1);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull Pitaija", 3.05, 'https://i.imgur.com/sGhyZAd.png', 'Green nature', '', '',  1);
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull Tropical", 2.95,'https://i.imgur.com/SxD0QBa.jpeg', 'Tropical sunshine', '','', 1);
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull Appelsiini", 2.95,'https://i.imgur.com/jBD3VCR.png', 'Fruity', '','', 1);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull Appelsiini", 2.90,'https://i.imgur.com/jBD3VCR.png', 'Fruity', '','', 1);
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Red Bull Vesimeloni", 2.95,'https://i.imgur.com/pWbRSWM.jpeg', 'Watermelony melody', '','', 1);
 
 
 
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery",  3.00, 'battery.png', 'Original','', '',  2);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery",  3.10, 'battery.png', 'Original','', '',  2);
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery Juiced",  3.00, 'https://i.imgur.com/vwrR5Yk.jpeg', 'Juicy','', '',  2);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery Juiced",  3.15, 'https://i.imgur.com/vwrR5Yk.jpeg', 'Juicy','', '',  2);
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery Licorice",  3.00, 'https://i.imgur.com/ESirhC5.jpeg', 'Licorice luikautus','', '',  2);
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery Remix",  3.00, 'https://i.imgur.com/LkdtSnc.jpeg', 'Remixed and mastered','', '',  2);
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery Jungled",  3.00, 'https://i.imgur.com/FaYKq0Q.jpeg', 'King of the jungle','', '',  2);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Battery Jungled",  2.95, 'https://i.imgur.com/FaYKq0Q.jpeg', 'King of the jungle','', '',  2);
 
 
 
 
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Monster",  2.95, 'https://i.imgur.com/4hEWwzm.jpg', 'Original','', '',  3);
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Monster Punch",  2.95, 'https://i.imgur.com/tpm2Txx.jpg', 'Punch your way out','', '',  3);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Monster Punch",  3.00, 'https://i.imgur.com/tpm2Txx.jpg', 'Punch your way out','', '',  3);
 
 INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Monster Juice",  2.95, 'https://i.imgur.com/NcQy0I2.jpg', 'Once again the juice versio','', '',  3);
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("https://i.imgur.com/GwJtTQH.png",  2.95, '', 'I dont even know','', '',  3);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("https://i.imgur.com/GwJtTQH.png",  2.85, '', 'I dont even know','', '',  3);
 
-INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Monster Ultra",  2.95, 'https://i.imgur.com/l33nTua.jpeg', 'The one and only','', '',  3);
+INSERT into product (name, price, image, kuvaus, ravintoarvot, aineosat, category_id) values ("Monster Ultra",  2.80, 'https://i.imgur.com/l33nTua.jpeg', 'The one and only','', '',  3);
 
 create table customer (
     id int primary key auto_increment,
